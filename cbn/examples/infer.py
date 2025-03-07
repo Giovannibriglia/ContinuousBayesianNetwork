fix exact inference output computation

cbn/examples/infer.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     # Infer CPDs for node C given evidence for A and B
     evidence = {
         "reward": torch.tensor([[1], [1], [1]], device="cuda"),
-        "action": torch.tensor([[2], [2], [1]], device="cuda"),
-        "obs_0": torch.tensor([[14], [14], [3]], device="cuda"),
+        "action": torch.tensor([[2], [2], [1], [3]], device="cuda"),
+        "obs_0": torch.tensor([[14], [14], [3], [2]], device="cuda"),
     }
     output = bn.infer(target_node, evidence, {})
     print(output)